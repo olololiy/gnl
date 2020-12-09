177615364c2c30eb178737209f162f166f509f4e
olololiy <olololiy@bk.ru>
2020-12-09 22:32:55 +0300

    f

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: vfurr <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 17:47:28 by vfurr             #+#    #+#             */
-/*   Updated: 2020/12/09 21:50:24 by vfurr            ###   ########.fr       */
+/*   Updated: 2020/12/09 22:32:49 by vfurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,6 @@ char	*ft_strjoinn(char const *s1, char **s2, int *i)
 	char	*b;
 
 	i = 0;
-//	j = 0;
-//	if (!s1 || !s2)
-//		return (0);
-//		o
 	lens1 = ft_strlenn(s1);
 	lens2 = ft_strlenn(*s2);
 	b = (char *)malloc(sizeof(char) * (lens1 + lens2 + 1));
