2cbffeb62aeaef015e8ed73b621c4766dfd9a800
olololiy <olololiy@bk.ru>
2020-12-11 00:06:51 +0300

    D

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: vfurr <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 15:29:15 by vfurr             #+#    #+#             */
-/*   Updated: 2020/12/09 22:31:33 by vfurr            ###   ########.fr       */
+/*   Updated: 2020/12/10 22:41:38 by vfurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#define BUFFER_SIZE 32
 
 int get_next_line(int fd, char **line)
 {
@@ -21,29 +22,24 @@ int get_next_line(int fd, char **line)
 	size	= 0;
 	i 		= 0;
 	if (!buf)
-	{
 		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		buf = NULL;
-	}
-	while ((*line)[i] == '\0')
+	while (*buf == '\0' || (*line)[i] == '\0')
 	{
-		write (1, "tut\n", 1);
-		if (buf && *buf != '\0')//излишнее условие
-			ft_strjoinn(*line, &buf, &i);
-		else
+        if(*buf == '\0')
 		{
-			buf = buf - size;
+			buf = buf - size;				//возможно эту строчку можно запихнуть в следующую
 			size =  read(fd, buf, BUFFER_SIZE);
-			if (size == (-1))
+			if (size == (-1))				
 				return (-1);
 			buf[size + 1] = '\0';
 		}
+		if (buf != NULL && *buf != '\0')	//излишнее условие//нет
+            line =	ft_strjoinn(*line, &buf, &i);
 	}
 	*line[i] = '\0';
 	if (size < BUFFER_SIZE)
 		return (0);
-	else
-		return (1);
+	return (1);
 }
 
 int main()
@@ -51,7 +47,7 @@ int main()
 	char *line;
 	int fd;
 
-	fd = open("text.txt", O_RDONLY);
+	fd = open("/Users/vfurr/workplace/gnl/text.txt", O_RDONLY);
 	printf("%d", get_next_line(fd, &line));
 	printf("%s\n", line);
 }
